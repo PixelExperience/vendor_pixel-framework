d16c187b6fb167b725164707f70bf44e706bd96d
jhenrique09 <jhsv09@gmail.com>
2022-09-03 16:32:15 -0300

    SystemUIGoogle: Update copyrights
    Change-Id: I3fcfc92355a7a207bd60a84d0dc865cbef8c835d

--- SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/theme/ThemeOverlayControllerGoogle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Pixel Experience Project
+ * Copyright (C) 2022 The PixelExperience Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
