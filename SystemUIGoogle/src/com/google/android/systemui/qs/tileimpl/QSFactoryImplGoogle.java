94355f6f4d40c39db4c3167bcbbb9103296356ac
Anay Wadhera <anay1018@gmail.com>
2022-12-07 01:28:02 +0900

    pixel-fw: Add Dream tile
    Reference: https://github.com/stx-staging/android_frameworks_base/commit/336d8fd21c3483dc49407639b268d6f87329a096
    
    Change-Id: I941da1f7f91989bebe7539739a6a7d4c718d850e

--- SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
+++ SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/QSFactoryImplGoogle.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DeviceControlsTile;
 import com.android.systemui.qs.tiles.DndTile;
+import com.android.systemui.qs.tiles.DreamTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.InternetTile;
@@ -100,6 +101,7 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
             Provider<QRCodeScannerTile> qrCodeScannerTileProvider,
             Provider<OneHandedModeTile> oneHandedModeTileProvider,
             Provider<ColorCorrectionTile> colorCorrectionTileProvider,
+            Provider<DreamTile> dreamTileProvider,
             Provider<ReverseChargingTile> reverseChargingTileProvider,
             Provider<PowerShareTile> powerShareTileProvider) {
         super(qsHostLazy,
@@ -133,6 +135,7 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
                 qrCodeScannerTileProvider,
                 oneHandedModeTileProvider,
                 colorCorrectionTileProvider,
+                dreamTileProvider,
                 powerShareTileProvider);
         mReverseChargingTileProvider = reverseChargingTileProvider;
         mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
