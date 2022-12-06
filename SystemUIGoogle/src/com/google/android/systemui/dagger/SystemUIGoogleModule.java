13171191e81cad3de676379c5b8a9a2fb4a9394f
Anay Wadhera <anay1018@gmail.com>
2022-12-07 01:23:37 +0900

    pixel-fw: Update BatteryControllerImpl constructor
    Change-Id: Iee224ec00e04b3ac7b9675cecb2d7a273f4b297f

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleModule.java
@@ -38,6 +38,7 @@ import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.dock.DockManagerImpl;
 import com.android.systemui.doze.DozeHost;
+import com.android.systemui.dump.DumpManager;
 import com.android.systemui.media.dagger.MediaModule;
 import com.android.systemui.navigationbar.gestural.GestureModule;
 import com.android.systemui.plugins.qs.QSFactory;
@@ -223,6 +224,7 @@ public abstract class SystemUIGoogleModule {
             PowerManager powerManager,
             BroadcastDispatcher broadcastDispatcher,
             DemoModeController demoModeController,
+            DumpManager dumpManager,
             @Main Handler mainHandler,
             @Background Handler bgHandler,
             UserContentResolverProvider userContentResolverProvider,
@@ -233,6 +235,7 @@ public abstract class SystemUIGoogleModule {
                 powerManager,
                 broadcastDispatcher,
                 demoModeController,
+                dumpManager,
                 mainHandler,
                 bgHandler,
                 userContentResolverProvider,
