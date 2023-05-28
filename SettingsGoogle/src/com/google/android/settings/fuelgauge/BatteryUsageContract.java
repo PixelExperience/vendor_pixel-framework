df5898eb963373c6ebadb65df75b25626984b7f3
Anay Wadhera <anay1018@gmail.com>
2023-05-28 16:54:57 -0400

    SettingsGoogle: add battery data provider
    Change-Id: I3bb7e1cde987433d18441628d7ffa1d1e9afe2f2

---
+++ SettingsGoogle/src/com/google/android/settings/fuelgauge/BatteryUsageContract.java
@@ -0,0 +1,4 @@
+package com.google.android.settings.fuelgauge;
+final class BatteryUsageContract {
+    static final String[] KEYS_BATTERY_USAGE_STATE = {"userId", "packageName", "percentage", "foregroundElapsedTime", "backgroundElapsedTime"};
+}
