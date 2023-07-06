f92d7bac4ff79e0c99c3a32a86c1f6e4baceb3df
someone5678 <nemui3353@gmail.com>
2023-07-06 12:44:22 +0900

    pixel-framework: Update SettingsGoogle lib
    * Based on previous PixelExperience's impl
    
    From:
    google/cheetah/cheetah:13/TQ3A.230705.001.A1/10217028:user/release-keys
    
    Change-Id: I53ca65b2810ca2a4add40ed71654646750bd7d4a
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- SettingsGoogle/src/com/google/android/settings/fuelgauge/BatteryUsageContract.java
+++
@@ -1,4 +0,0 @@
-package com.google.android.settings.fuelgauge;
-final class BatteryUsageContract {
-    static final String[] KEYS_BATTERY_USAGE_STATE = {"userId", "packageName", "percentage", "foregroundElapsedTime", "backgroundElapsedTime"};
-}
